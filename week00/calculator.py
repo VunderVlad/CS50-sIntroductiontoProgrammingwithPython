2db92cab3c57944370f7703fee944870d6989cbe
Vladoo <118483064+VunderVlad@users.noreply.github.com>
2026-02-02 11:39:03 +0100

    Study session 1

---
+++ week00/calculator.py
@@ -0,0 +1,8 @@
+def main():
+    x = int(input("What is x? "))
+    print("x squered is", squere(x))
+
+def squere(n):
+    return n * n
+
+main()
---
+++ week00/hello.py
@@ -0,0 +1,11 @@
+def main():
+    hello()
+    name = input("What is your name? ")
+    hello(name)
+
+
+def hello(to="world"):
+    print("Hello,", to )
+
+
+main()
